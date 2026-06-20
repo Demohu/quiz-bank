d02b4fedec2712be3daf76adfee17ea8b9ecf76a
liawb <liawb@example.com>
2026-06-20 11:44:51 +0800

    chore: cleanup repository clutter and ignore scratch/temp files

--- scratch/print_initial_state.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-path = 'C:/Users/liawb/OneDrive/Desktop/題庫/index.html'
-with open(path, 'r', encoding='utf-8') as f:
-    html = f.read()
-
-idx = html.find('const initialState = {')
-if idx != -1:
-    sys.stdout.buffer.write(html[idx:idx+800].encode('utf-8'))
--- scratch/print_weight_display_context.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-path = 'C:/Users/liawb/OneDrive/Desktop/題庫/index.html'
-with open(path, 'r', encoding='utf-8') as f:
-    html = f.read()
-
-idx = html.find('權重 {w.toFixed(0)}')
-if idx != -1:
-    sys.stdout.buffer.write(html[idx-1000:idx+300].encode('utf-8'))
